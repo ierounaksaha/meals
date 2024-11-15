f0bb1465c2e787f6fe4984952c94d8e777a4675d
Rounak Saha <i.e.rounaksaha@gmail.com>
2024-11-15 17:01:48 +0530

    Final Commit

---
+++ lib/screens/filters.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+enum Filter{
+  glutenFree,
+  lactosFree,
+  vegetarian,
+  vegan,
+}
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key,required this.currentFilters});
+
+  final Map<Filter,bool> currentFilters;
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+
+  var _glutenFreeFilterSet = false;
+  var _lactosFreeFilterSet = false;
+  var _vegetarianFilterSet = false;
+  var _veganFilterSet = false;
+
+  @override
+  void initState() {
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _vegetarianFilterSet = widget.currentFilters[Filter.vegetarian]!;
+    _lactosFreeFilterSet = widget.currentFilters[Filter.lactosFree]!;
+    _veganFilterSet = widget.currentFilters[Filter.vegan]!;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, dynamic result){
+          if(didPop) return;
+          Navigator.of(context).pop(
+            {
+              Filter.glutenFree : _glutenFreeFilterSet,
+              Filter.lactosFree : _lactosFreeFilterSet,
+              Filter.vegetarian : _vegetarianFilterSet,
+              Filter.vegan : _veganFilterSet,
+            }
+          );
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (newValue){
+                setState(() {
+                  _glutenFreeFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Gluten-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              subtitle: Text('Only include gluten-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34,right: 22),
+            ),
+            SwitchListTile(
+              value: _lactosFreeFilterSet,
+              onChanged: (newValue){
+                setState(() {
+                  _lactosFreeFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Lactos-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              subtitle: Text('Only include lactos-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34,right: 22),
+            ),
+            SwitchListTile(
+              value: _vegetarianFilterSet,
+              onChanged: (newValue){
+                setState(() {
+                  _vegetarianFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Vegetarian',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              subtitle: Text('Only include vegetarian meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34,right: 22),
+            ),
+            SwitchListTile(
+              value: _veganFilterSet,
+              onChanged: (newValue){
+                setState(() {
+                  _veganFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Vegan',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              subtitle: Text('Only include vegan meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34,right: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({super.key, required this.meal, required this.onToggleFavorites, required this.favoriteMeals});
+
+  final Meal meal;
+  final void Function(Meal meal) onToggleFavorites;
+  final List<Meal> favoriteMeals;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final isFavorite = favoriteMeals.contains(meal);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(onPressed: (){
+            onToggleFavorites(meal);
+          },
+          icon:Icon(isFavorite? Icons.star: Icons.star_border),),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 14,),
+            for (final ingrediant in meal.ingredients)
+              Text(
+                ingrediant,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+        
+            const SizedBox(height: 24,),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 14,),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
